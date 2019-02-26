cf399079c4e22a0707bf2ec1ed7964be27d02e79
daleksprinter <b1015120@fun.ac.jp>
2019-02-27 04:41:51 +0900

    add tweet button

--- src/userdata.js
+++ src/userdata.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Paper from '@material-ui/core/Paper';
 import './userdata.css';
 import ACData from './acdata.js';
+import { SocialIcon } from 'react-social-icons';
+
+
 export default class UserData extends Component{
 
     render(){
@@ -11,6 +14,9 @@ export default class UserData extends Component{
                 {Object.keys(this.props.data).map((key) => (
                     <ACData className = "account" site = {key} count = {this.props.data[key]} />
                 ))}
+                <button className = 'tweetbutton' onClick = {() => (alert("未実装です。ごめんなさい。"))}>
+                    <SocialIcon network = "twitter"/>
+                </button>
             </Paper>
         )
     }
