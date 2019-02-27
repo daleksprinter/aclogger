ad1c7c9b554fd84754b6fdb5e3c92b6079df62f6
daleksprinter <b1015120@fun.ac.jp>
2019-02-27 10:40:26 +0900

    add tweet function

--- src/userdata.js
+++ src/userdata.js
@@ -8,15 +8,20 @@ import { SocialIcon } from 'react-social-icons';
 export default class UserData extends Component{
 
     render(){
+        var tweettext = "";
+        for(const k in this.props.data){
+            tweettext += k + " : " + this.props.data[k] + "AC" + "%0A";
+        }
+        tweettext += "Send by ACLogger";
         return(
             <Paper className = 'userdata'>
 
                 {Object.keys(this.props.data).map((key) => (
                     <ACData className = "account" site = {key} count = {this.props.data[key]} />
                 ))}
-                <button className = 'tweetbutton' onClick = {() => (alert("未実装です。ごめんなさい。"))}>
-                    <SocialIcon network = "twitter"/>
-                </button>
+                <a className = 'tweetbutton' href={"https://twitter.com/intent/tweet?text=" + tweettext}>
+                    <SocialIcon network = 'twitter' />
+                </a>
             </Paper>
         )
     }
