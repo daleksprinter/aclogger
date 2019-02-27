763294f8a49db2adaf0b51557cb6d89fa3f4ffcd
daleksprinter <b1015120@fun.ac.jp>
2019-02-27 11:00:02 +0900

    何更新したか忘れた

--- src/aboutpage.js
+++ src/aboutpage.js
@@ -11,13 +11,11 @@ export default class AboutPage extends Component{
                     <div>&emsp;競技プログラミングのコンテストサイトやジャッジサイト等で問題を解くことで、草を生やすことができるツールです。</div>
                     <div>&emsp;競プロと草を生やすことが好きなみなさんのために作成されました。</div>
                     <div>&emsp;精進の記録や、モチベーション維持等にお役立てください。</div>
-                    <div>&emsp;現状、AtCoderとCodeforcesに対応しています。</div>
+                    <div>&emsp;現状、AtCoder,Codeforces,AOJに対応しています。</div>
                 <br></br>
                 <div>今後の更新予定</div>
-                    <div>&emsp;・ジャッジサイトの追加(AOJ, yukicoder, その他APIが生えているジャッジ等)</div>
-                    <div>&emsp;・ジャッジごと(及び合計）のACカウントの表示</div>
+                    <div>&emsp;・ジャッジサイトの追加(APIが提供されていれば)</div>
                     <div>&emsp;・Daily AC, 合計ACのツイート（及び自動化）</div>
-                    <div>&emsp;・提出詳細に飛べるようにする</div>
                     <div>&emsp;・ユーザーネームをcookieに保存</div>
                 <br></br>
                 <div>作った人</div>
