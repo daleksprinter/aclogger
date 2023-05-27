34d74eabbf8511f76911db6acd6e2586d8f12325
r-sekine <ca01376@cartahd.com>
2023-05-27 23:04:10 +0900

    fix

--- src/modules/clientImplements/yukicoderClient.ts
+++ src/modules/clientImplements/yukicoderClient.ts
@@ -5,9 +5,12 @@ import { Client } from "../client";
 export class yukicoderClient implements Client {
   user: String;
   url: string;
+  parser: yukicoderResponseParser;
   constructor(user: String) {
     this.user = user;
+    // @see https://petstore.swagger.io/?url=https://yukicoder.me/api/swagger.yaml#/user/get_v1_solved__param___user_
     this.url = `https://yukicoder.me/api/v1/solved/name/${this.user}`;
+    this.parser = new yukicoderResponseParser();
   }
   fetch() {
     if (this.user !== "" && this.user !== undefined) {
@@ -21,12 +24,52 @@ export class yukicoderClient implements Client {
 
   getAllSubmissions() {
     return this.fetch().then((json) => {
-      const subs = this.toSubmissions(json);
+      const subs = this.parser.toSubmissions(json);
       return Promise.resolve(subs);
     });
   }
+}
 
-  resToSub(res: any) {
+/* Response Sample
+[
+  {
+    "No": 1,
+    "ProblemId": 17,
+    "Title": "道のショートカット",
+    "AuthorId": 10,
+    "TesterIds": "",
+    "Level": 3,
+    "ProblemType": 0,
+    "Tags": "動的計画法",
+    "Date": "2018-09-20T13:51:28+09:00"
+  },
+  {
+    "No": 3,
+    "ProblemId": 11,
+    "Title": "ビットすごろく",
+    "AuthorId": 10,
+    "TesterIds": "",
+    "Level": 2,
+    "ProblemType": 0,
+    "Tags": "幅優先探索",
+    "Date": "2018-08-12T09:44:19+09:00"
+  },
+  {
+    "No": 4,
+    "ProblemId": 19,
+    "Title": "おもりと天秤",
+    "AuthorId": 10,
+    "TesterIds": "",
+    "Level": 2.5,
+    "ProblemType": 0,
+    "Tags": "動的計画法",
+    "Date": "2018-09-02T00:56:10+09:00"
+  }
+]
+ */
+
+class yukicoderResponseParser {
+  private resToSub(res: any) {
     const subtime = new Date(res["Date"]).getTime();
     const result = statusfactory.Accept();
     const contestid = null;
@@ -51,4 +94,5 @@ export class yukicoderClient implements Client {
     }
     return subs;
   }
+
 }
