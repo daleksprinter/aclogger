19775bc56a54abd0eaf50ad8dc61bfb64fd6c46c
r-sekine <ca01376@cartahd.com>
2023-05-27 21:40:11 +0900

    fix

--- src/components/SubmissionCounts/SubmissionCount.tsx
+++ src/components/SubmissionCounts/SubmissionCount.tsx
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 interface AppProps{
     site: string
     count: number
 }
-interface AppState{}
-export default class SubmissionCount extends Component<AppProps, AppState>{
+const SubmissionCount = (props: AppProps) => {
 
-    render(){
-        return(
-            <div style={{display:'inline-block', margin:'20px 30px', color:'rgb(100, 100, 100)'}}>
-                <div>{this.props.site}</div>
-                <div style={{fontSize:'34px', fontWeight:'bold'}}>{this.props.count}</div>
-            </div>
-        )
-    }
+    return(
+        <div style={{display:'inline-block', margin:'20px 30px', color:'rgb(100, 100, 100)'}}>
+            <div>{props.site}</div>
+            <div style={{fontSize:'34px', fontWeight:'bold'}}>{props.count}</div>
+        </div>
+    )
 }
+
+export default SubmissionCount
--- src/components/SubmissionCounts/SubmissionCounts.tsx
+++ src/components/SubmissionCounts/SubmissionCounts.tsx
@@ -7,19 +7,15 @@ import {Submissions} from "../../modules/submit";
 interface AppProps{
     data: Submissions
 }
-interface AppState{}
-export default class SubmissionCounts extends Component<AppProps, AppState>{
-
-    render(){
-        return(
-            <Paper style={{textAlign:'center', position:'relative', marginBottom:'1%'}}>
-                <SubmissionCount site = {"AtCoder"} count = {this.props.data.count(sitefactory.AtCoder())} />
-                <SubmissionCount site = {"Codeforces"} count = {this.props.data.count(sitefactory.Codeforces())} />
-                <SubmissionCount site = {"AOJ"} count = {this.props.data.count(sitefactory.AOJ())} />
-                <SubmissionCount site = {"yukicoder"} count = {this.props.data.count(sitefactory.yukicoder())} />
-                <SubmissionCount site = {"Sum"} count = {this.props.data.count(null)} />
-            </Paper>
-        )
-    }
-    
+const SubmissionCounts = (props: AppProps) => {
+    return(
+        <Paper style={{textAlign:'center', position:'relative', marginBottom:'1%'}}>
+            <SubmissionCount site = "AtCoder" count = {props.data.count(sitefactory.AtCoder())} />
+            <SubmissionCount site = "Codeforces" count = {props.data.count(sitefactory.Codeforces())} />
+            <SubmissionCount site = "AOJ" count = {props.data.count(sitefactory.AOJ())} />
+            <SubmissionCount site = "yukicoder" count = {props.data.count(sitefactory.yukicoder())} />
+            <SubmissionCount site = "Sum" count = {props.data.count(null)} />
+        </Paper>
+    )
 }
+export default SubmissionCounts
