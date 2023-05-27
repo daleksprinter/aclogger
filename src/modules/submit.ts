6475863a69f864bcf44ece18be5d01ded691bc2f
r-sekine <ca01376@cartahd.com>
2023-05-27 14:29:40 +0900

    fix

--- src/modules/submit.ts
+++ src/modules/submit.ts
@@ -1,92 +1,100 @@
-import {getdate} from "./utils";
-import {sitefactory, Site} from "./site";
-import {Status} from "./status"
-import {Conditions} from "./condition";
+import { getdate } from "./utils";
+import { sitefactory, Site } from "./site";
+import { Status } from "./status";
+import { Conditions } from "./condition";
 
 export abstract class Submit {
-    timestamp: number
-    result: Status
-    contest: String | null
-    title: String
-    point: number | null
-    url:string
-    constructor(timestamp: number, result: Status, contest: String | null, title: String, point: number | null, url: string) {
-        this.timestamp = timestamp
-        this.result = result
-        this.contest  = contest
-        this.title = title
-        this.point = point
-        this.url = url
-    }
+  timestamp: number;
+  result: Status;
+  contest: String | null;
+  title: String;
+  point: number | null;
+  url: string;
+  constructor(
+    timestamp: number,
+    result: Status,
+    contest: String | null,
+    title: String,
+    point: number | null,
+    url: string
+  ) {
+    this.timestamp = timestamp;
+    this.result = result;
+    this.contest = contest;
+    this.title = title;
+    this.point = point;
+    this.url = url;
+  }
 
-    getResult() {
-        return this.result
-    }
-    getPoint(){
-        return this.point
-    }
+  getResult() {
+    return this.result;
+  }
+  getPoint() {
+    return this.point;
+  }
 
-    getDateString() {
-        return getdate(this.timestamp)
-    }
+  getDateString() {
+    return getdate(this.timestamp);
+  }
 
-    getDate(){
-        return new Date(this.timestamp)
-    }
+  getDate() {
+    return new Date(this.timestamp);
+  }
 
-    abstract getSite(): Site
+  abstract getSite(): Site;
 }
 
 export class AtCoderSubmit extends Submit {
-    getSite() {
-        return sitefactory.AtCoder()
-    }
+  getSite() {
+    return sitefactory.AtCoder();
+  }
 }
 export class CodeforcesSubmit extends Submit {
-    getSite() {
-        return sitefactory.Codeforces()
-    }
+  getSite() {
+    return sitefactory.Codeforces();
+  }
 }
 export class AOJSubmit extends Submit {
-     getSite() {
-         return sitefactory.AOJ()
-    }
+  getSite() {
+    return sitefactory.AOJ();
+  }
 }
 export class yukicoderSubmit extends Submit {
-     getSite() {
-         return sitefactory.yukicoder()
-    }
+  getSite() {
+    return sitefactory.yukicoder();
+  }
 }
 
 export class Submissions {
-    subs: Submit[]
-    constructor(subs: Submit[] | null) {
-        this.subs = subs ? subs : []
-    }
+  subs: Submit[];
+  constructor(subs: Submit[] | null) {
+    this.subs = subs ? subs : [];
+  }
 
-    add(submission: Submit) {
-        this.subs.push(submission)
-    }
+  add(submission: Submit) {
+    this.subs.push(submission);
+  }
 
-    count(site: Site | null) {
-        if(site) return this.getAll().filter(sub => sub.getSite().isSame(site)).length
-        return this.getAll().length
-    }
+  count(site: Site | null) {
+    if (site)
+      return this.getAll().filter((sub) => sub.getSite().isSame(site)).length;
+    return this.getAll().length;
+  }
 
-    getAll()  {
-        return this.subs
-    }
+  getAll() {
+    return this.subs;
+  }
 
-    merge(submissions: Submissions){
-        submissions.getAll().map(sub => {
-            this.add(sub)
-        })
-        return this
-    }
+  merge(submissions: Submissions) {
+    submissions.getAll().map((sub) => {
+      this.add(sub);
+    });
+    return this;
+  }
 
-    filter(conditions: Conditions) {
-        return this.getAll().filter(sub => {
-            return conditions.accept(sub)
-        })
-    }
+  filter(conditions: Conditions) {
+    return this.getAll().filter((sub) => {
+      return conditions.accept(sub);
+    });
+  }
 }
--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,8 +1,14 @@
-function zeroPadding(num: number, len: number): string{
-  return ('00000' + num).slice(-len);
+function zeroPadding(num: number, len: number): string {
+  return ("00000" + num).slice(-len);
 }
 
-export function getdate(millisec: number) : string{
+export function getdate(millisec: number): string {
   const d = new Date(millisec);
-  return d.getFullYear() + "-" + zeroPadding(d.getMonth() + 1, 2) + "-" + zeroPadding(d.getDate(), 2);
+  return (
+    d.getFullYear() +
+    "-" +
+    zeroPadding(d.getMonth() + 1, 2) +
+    "-" +
+    zeroPadding(d.getDate(), 2)
+  );
 }
