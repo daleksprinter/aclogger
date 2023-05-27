75f5536719a5a8787a69b04e0cfcfa94ec281006
r-sekine <ca01376@cartahd.com>
2023-05-27 17:28:21 +0900

    use_template_literal

--- src/modules/client.ts
+++ src/modules/client.ts
@@ -14,7 +14,7 @@ export class AtCoderClient extends BaseClient {
   constructor(user: String) {
     super();
     this.user = user;
-    this.url = "https://kenkoooo.com/atcoder/atcoder-api/results?user=" + user;
+    this.url = `https://kenkoooo.com/atcoder/atcoder-api/results?user=${user}`;
   }
   fetch() {
     if (this.user !== "" && this.user !== undefined) {
@@ -51,11 +51,7 @@ export class AtCoderClient extends BaseClient {
     const contestid = res["contest_id"].toUpperCase();
     const title = res["problem_id"];
     const point = res["point"];
-    const url =
-      "https://atcoder.jp/contests/" +
-      res["contest_id"] +
-      "/submissions/" +
-      res["id"];
+    const url = `https://atcoder.jp/contests/${res["contest_id"]}/submissions/${res["id"]}`;
 
     const s = new AtCoderSubmit(subtime, result, contestid, title, point, url);
     return s;
@@ -76,10 +72,7 @@ export class CodeForcesClient extends BaseClient {
   constructor(user: String) {
     super();
     this.user = user;
-    this.url =
-      "https://codeforces.com/api/user.status?handle=" +
-      this.user +
-      "&from=1&count=1000";
+    this.url = `https://codeforces.com/api/user.status?handle=${this.user}&from=1&count=1000`;
   }
 
   fetch() {
@@ -148,10 +141,7 @@ export class AizuOnlineJudgeClient extends BaseClient {
   constructor(user: String) {
     super();
     this.user = user;
-    this.url =
-      "https://judgeapi.u-aizu.ac.jp/submission_records/users/" +
-      this.user +
-      "?page=0&size=10000";
+    this.url = `https://judgeapi.u-aizu.ac.jp/submission_records/users/${this.user}?page=0&size=10000`;
   }
   fetch() {
     if (this.user !== "" && this.user !== undefined) {
@@ -185,8 +175,7 @@ export class AizuOnlineJudgeClient extends BaseClient {
     const contestid = null;
     const title = res["problemId"];
     const point = null;
-    const url =
-      "http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=" + res["judgeId"];
+    const url = `http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=${res["judgeId"]}`;
     const s = new AOJSubmit(subtime, result, contestid, title, point, url);
     return s;
   }
@@ -206,7 +195,7 @@ export class yukicoderClient extends BaseClient {
   constructor(user: String) {
     super();
     this.user = user;
-    this.url = "https://yukicoder.me/api/v1/solved/name/" + this.user;
+    this.url = `https://yukicoder.me/api/v1/solved/name/${this.user}`;
   }
   fetch() {
     if (this.user !== "" && this.user !== undefined) {
@@ -231,7 +220,7 @@ export class yukicoderClient extends BaseClient {
     const contestid = null;
     const title = res["Title"];
     const point = res["Level"];
-    const url = "https://yukicoder.me/";
+    const url = `https://yukicoder.me/`;
     const s = new yukicoderSubmit(
       subtime,
       result,
